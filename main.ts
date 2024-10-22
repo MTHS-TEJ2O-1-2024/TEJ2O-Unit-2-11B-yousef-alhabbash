b124e6f37b31aa9790cbca382ef7c585c5c66001
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-10-22 11:09:51 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: OCT 2024
+ * This program generate random int from 0-99 and sees which one is bigger by shaking
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
+
+let firstNumber = randint(0,99)
+let secondNumber = randint(0, 99)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showString("#1:");
+    basic.showNumber(firstNumber)
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showString("#2:");
+    basic.showNumber(secondNumber);
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onGesture(Gesture.Shake, function () {
+    if (firstNumber < secondNumber) {
+        basic.showString(firstNumber + " < " + secondNumber);
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showString(firstNumber + " > " + secondNumber);
+        basic.showIcon(IconNames.Sad)
+    }
+    basic.showIcon(IconNames.Happy)
+})
